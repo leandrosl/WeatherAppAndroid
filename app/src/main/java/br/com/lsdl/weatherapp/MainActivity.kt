33b3784f46b3cf7b560a0f8682a2ebcf6c7fb540
Leandro Souza <sl.leandro09@gmail.com>
2024-05-16 20:50:01 -0300

    Limpando codigo nao utilizado na MainActivity

--- app/src/main/java/br/com/lsdl/weatherapp/MainActivity.kt
+++ app/src/main/java/br/com/lsdl/weatherapp/MainActivity.kt
@@ -1,27 +1,17 @@
 package br.com.lsdl.weatherapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import br.com.lsdl.weatherapp.fragments.ShowTemperatureFragment
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val temperatureFragment = ShowTemperatureFragment()
-
         supportFragmentManager
             .beginTransaction()
-            .add(R.id.main_activity_fragment_view, temperatureFragment)
+            .add(R.id.main_activity_fragment_view, ShowTemperatureFragment())
             .commit()
-
-        //Fazer o botao da tela inicial abrir a tela de pesquisa
-        /*findViewById<FloatingActionButton>(R.id.searchButton).setOnClickListener {
-            startActivity(Intent(applicationContext, ListCitiesActivity::class.java))
-        }*/
     }
 }
