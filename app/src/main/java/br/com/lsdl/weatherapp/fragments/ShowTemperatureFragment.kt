62a4df240f180c6bac5000c9ccb82242287a62ee
Leandro Souza <sl.leandro09@gmail.com>
2024-05-16 20:49:33 -0300

    Adicionando listener do botao no fragment principal para abrir o fragment de listagem de cidades

--- app/src/main/java/br/com/lsdl/weatherapp/fragments/ShowTemperatureFragment.kt
+++ app/src/main/java/br/com/lsdl/weatherapp/fragments/ShowTemperatureFragment.kt
@@ -6,13 +6,31 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import br.com.lsdl.weatherapp.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ShowTemperatureFragment : Fragment() {
+    private lateinit var searchButton: FloatingActionButton
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_show_temperature, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        searchButton = view.findViewById(R.id.searchButton)
+
+        searchButton.setOnClickListener {
+            parentFragmentManager
+                .beginTransaction()
+                .setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+                .replace(R.id.main_activity_fragment_view, SearchCityFragment())
+                .addToBackStack(null)
+                .commit()
+        }
     }
 }
