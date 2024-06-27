740a4789affd8b0595f6f17897539e5466a09cbd
Leandro Souza <sl.leandro09@gmail.com>
2024-06-27 20:55:40 -0300

    Incluindo botao voltar e edittext na toolbar da tela de cidades

--- app/src/main/java/br/com/lsdl/weatherapp/fragments/SearchCityFragment.kt
+++ app/src/main/java/br/com/lsdl/weatherapp/fragments/SearchCityFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -23,6 +25,14 @@ class SearchCityFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val activity = requireActivity() as AppCompatActivity
+
+        val fragmentToolbar = view.findViewById<Toolbar>(R.id.toolbar)
+
+        activity.setSupportActionBar(fragmentToolbar)
+        activity.supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        activity.supportActionBar?.setDisplayShowTitleEnabled(false)
 
         initListCitiesView(view)
     }
