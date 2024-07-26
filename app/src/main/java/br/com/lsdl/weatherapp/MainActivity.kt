95af6d3c0eae68a5549ba6f3fbe854382c70d130
Leandro Souza <sl.leandro09@gmail.com>
2024-07-25 21:09:21 -0300

    Opção de configurações para a toolbar

--- app/src/main/java/br/com/lsdl/weatherapp/MainActivity.kt
+++ app/src/main/java/br/com/lsdl/weatherapp/MainActivity.kt
@@ -1,6 +1,9 @@
 package br.com.lsdl.weatherapp
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import br.com.lsdl.weatherapp.fragments.ShowTemperatureFragment
 
@@ -13,5 +16,22 @@ class MainActivity : AppCompatActivity() {
             .beginTransaction()
             .add(R.id.main_activity_fragment_view, ShowTemperatureFragment())
             .commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.fragment_search_city_toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_app_settings -> {
+                Toast.makeText(applicationContext, "Settings", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
+        }
     }
 }
