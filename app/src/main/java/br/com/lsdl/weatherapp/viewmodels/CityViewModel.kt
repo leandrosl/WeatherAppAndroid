cc0b8ae2c598cd7e91708cc8234c3539dfe90b65
Leandro Souza <sl.leandro09@gmail.com>
2024-12-12 21:34:53 -0300

    Adicionando funcao de filtrar cidade pelo nome no viewModel

--- app/src/main/java/br/com/lsdl/weatherapp/viewmodels/CityViewModel.kt
+++ app/src/main/java/br/com/lsdl/weatherapp/viewmodels/CityViewModel.kt
@@ -5,14 +5,26 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import br.com.lsdl.weatherapp.models.City
 import br.com.lsdl.weatherapp.repositories.CityRepository
+import kotlinx.coroutines.runBlocking
 
 class CityViewModel(private val repository: CityRepository) : ViewModel() {
-    val cities: LiveData<List<City>> get() = repository.cities
+    private val _cities = MutableLiveData <List<City>>()
+    val cities: LiveData<List<City>> = _cities
 
     private val _selectedCity = MutableLiveData<City?>()
     val selectedCity: LiveData<City?> = _selectedCity
 
+    init {
+        _cities.value = repository.getCities()
+    }
+
     fun selectCity(city: City) {
         _selectedCity.value = city
+    }
+
+    fun filterCitiesByName(name: String) {
+        runBlocking {
+           _cities.value = repository.getCitiesByName(name)
+        }
     }
 }
