f25bc72c8f8b185daeb3b9142bcc737d4da8f6b7
Leandro Souza <sl.leandro09@gmail.com>
2025-01-30 20:31:45 -0300

    Permitindo enviar texto vazio de pesquisa de cidade para a viewModel

--- app/src/main/java/br/com/lsdl/weatherapp/fragments/SearchCityFragment.kt
+++ app/src/main/java/br/com/lsdl/weatherapp/fragments/SearchCityFragment.kt
@@ -78,9 +78,7 @@ class SearchCityFragment : Fragment() {
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
             override fun afterTextChanged(p0: Editable?) {
-                if (toolbarSearchEditText.text.toString().length > 1) {
-                    cityViewModel.filterCitiesByName(toolbarSearchEditText.text.toString())
-                }
+                cityViewModel.filterCitiesByName(toolbarSearchEditText.text.toString())
             }
         })
     }
