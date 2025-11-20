8675f6a5a9eb5911840077ff05d14a8167746279
Leandro Souza <sl.leandro09@gmail.com>
2025-11-20 20:56:20 -0300

    Colocando Scaffold como elemento principal da tela de exibir temperatura

--- app/src/main/java/br/com/lsdl/weatherapp/screens/ShowTemperatureScreen.kt
+++ app/src/main/java/br/com/lsdl/weatherapp/screens/ShowTemperatureScreen.kt
@@ -1,9 +1,41 @@
 package br.com.lsdl.weatherapp.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun ShowTemperatureScreen() {
-    Text("Temperature")
+    Scaffold(
+        containerColor = Color(0xFF448AFF)
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .padding(innerPadding)
+        ) {
+            Text(
+                text = "Belo Horizonte",
+                color = Color.White,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                fontSize = 24.sp,
+                modifier = Modifier
+                    .padding(top = 16.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+        }
+    }
 }
